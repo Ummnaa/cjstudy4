242b81b2a83eb668c06ecd1a313aebc17318889e
neonite-dev <110008485+neonite-dev@users.noreply.github.com>
2022-11-24 16:53:09 +0900

    Add files via upload

---
+++ src/main/java/com/example/chunjae/cjstudy4/Cjstudy4Application.java
@@ -0,0 +1,13 @@
+package com.example.chunjae.cjstudy4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cjstudy4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cjstudy4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chunjae/cjstudy4/Cjstudy4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chunjae.cjstudy4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cjstudy4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
